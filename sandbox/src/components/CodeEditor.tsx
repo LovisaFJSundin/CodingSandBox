a869446275caa4c8a1356adb6af366e9aca42b1a
2092502s <lovisafsundin@gmail.com>
2021-11-17 21:17:38 +0100

    Added syntax highlighting

--- sandbox/src/components/CodeEditor.tsx
+++ sandbox/src/components/CodeEditor.tsx
@@ -1,5 +1,12 @@
 import React, { useRef} from 'react'
+import './syntax.css'
 import MonacoEditor, { OnMount } from '@monaco-editor/react'
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel'
+import codeShift from 'jscodeshift'
+import Highlighter from 'monaco-jsx-highlighter'
+import {parse} from '@babel/parser'
+import traverse from '@babel/traverse'
 
 interface CodeEditorProps {
     initialValue: string;
@@ -14,9 +21,34 @@ const CodeEditor : React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
         editorRef.current = editor;
         editor.onDidChangeModelContent(()=> onChange(editor.getValue()));
         editor.getModel()?.updateOptions({ tabSize: 2});
+
+        const highlighter = new Highlighter(
+            //@ts-ignore
+            window.monaco,
+            parse, 
+            traverse, 
+            editor
+        );
+        highlighter.highLightOnDidChangeModelContent(100);
+        highlighter.addJSXCommentCommand();
+    }
+
+    const onFormatClick = () => {
+        const unformatted = editorRef.current.getModel().getValue();
+        const formatted = prettier.format(unformatted, {
+            parser: 'babel',
+            plugins: [parser],
+            useTabs: false,
+            semi: true,
+            singleQuote: true
+        }).replace(/\n$/,'');
+
+        editorRef.current.setValue(formatted);
     }
 
     return (
+        <>
+        <button onClick={onFormatClick}>Format</button>
         <MonacoEditor 
         onMount={onEditorMount}
         value={initialValue}
@@ -33,6 +65,7 @@ const CodeEditor : React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
             automaticLayout: true
         }}
         />
+        </>
     )
 }
 
